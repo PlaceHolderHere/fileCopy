3f2769bf68d2f760f9921fc9c6a75d2716715aec
PlaceHolderHere <holderhereinplace@gmail.com>
2026-04-23 10:58:55 +0800

    Added a version of copyDir that does not output/print logs of the files and directories being copied, logs can be reactivated with the -l flag as a 5th argument

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 
 int copyFile(char filePath[], char destinationPath[]);
 int copyDir(char referenceDirectoryPath[], char destinationDirectoryPath[]);
+int copyDirLogless(char referenceDirectoryPath[], char destinationDirectoryPath[]);
 
 int main(int argc, char *argv[]){
     if (argc < 3){
@@ -23,13 +24,27 @@ int main(int argc, char *argv[]){
     if (argc > 3){
         outputName = argv[3];
     }
-    char outputPath[1024];
+    char *logFlag;
+    if (argc > 4){
+        logFlag = argv[4];
+    }
 
     // Updating the output file path
+    char outputPath[1024];
     snprintf(outputPath, sizeof(outputPath), "%s\\%s", outputDirectoryPath, outputName);
-    if (copyDir(referenceDirectoryPath, outputPath) != 0){
-        printf("Error! Could not copy directory.\n");
-        return -1;
+    
+    // Checking for log flag
+    if (strcmp(logFlag, "-l") == 0){
+        if (copyDir(referenceDirectoryPath, outputPath) != 0){
+            printf("Error! Could not copy directory.\n");
+            return -1;
+        }
+    }
+    else{
+        if (copyDirLogless(referenceDirectoryPath, outputPath) != 0){
+            printf("Error! Could not copy directory.\n");
+            return -1;
+        }
     }
 
     return 0;
@@ -86,6 +101,70 @@ int copyDir(char referenceDirectoryPath[], char destinationDirectoryPath[]){
                 // File Copying
                 else if(S_ISREG(currentFileInfo.st_mode)){ 
                     printf("Copying %s...\n", refDirEntry->d_name);
+                    if (copyFile(currentFilePath, outputFilePath) != 0){
+                        printf("Error! Failed to copy %s\n", refDirEntry->d_name);
+                        returnValue = -1;
+                        goto closeRefDir;
+                    }   
+                }   
+            }
+        }
+    }
+
+    closeRefDir:
+    closedir(referenceDirectory);
+    return returnValue;
+}
+
+
+int copyDirLogless(char referenceDirectoryPath[], char destinationDirectoryPath[]){
+    // Variable Initialization
+    int returnValue = 0;
+    struct dirent *refDirEntry;
+    struct stat currentFileInfo;
+    struct stat outputFileInfo;
+    char currentFilePath[1024];
+    char outputFilePath[1024];
+
+    DIR *referenceDirectory = opendir(referenceDirectoryPath);
+    if (referenceDirectory == NULL){
+        printf("Error! Failed to open referenceDirectory.\n");
+        returnValue = -1;
+        goto closeRefDir;
+    }
+
+    // Checking if output directory exists
+    if (stat(destinationDirectoryPath, &outputFileInfo) != 0){
+        if (errno == ENOENT){
+            if (mkdir(destinationDirectoryPath) != 0){
+                printf("Error! Failed to copy directory.\n");
+                returnValue = -1;
+                goto closeRefDir;
+            }
+        }
+        else{
+            printf("Error! Could not Access the output directory.\n");
+            returnValue = -1;
+            goto closeRefDir;
+        }
+    }
+
+    // Reading the Reference Directory
+    while ((refDirEntry = readdir(referenceDirectory)) != NULL){
+        if (strcmp(refDirEntry->d_name, "..") != 0 && strcmp(refDirEntry->d_name, ".")){ // Filtering Out Parent & Current Working Directory
+            snprintf(currentFilePath, sizeof(currentFilePath), "%s\\%s", referenceDirectoryPath, refDirEntry->d_name); // currentFilePath
+            snprintf(outputFilePath, sizeof(outputFilePath), "%s\\%s", destinationDirectoryPath, refDirEntry->d_name); // outputFilePath
+            
+            // Separating Directories and Files
+            if (stat(currentFilePath, &currentFileInfo) == 0){
+                if (S_ISDIR(currentFileInfo.st_mode)){
+                    if (copyDir(currentFilePath, outputFilePath) != 0){
+                        returnValue = -1;
+                        goto closeRefDir;
+                    }
+                }
+                // File Copying
+                else if(S_ISREG(currentFileInfo.st_mode)){ 
                     if (copyFile(currentFilePath, outputFilePath) != 0){
                         printf("Error! Failed to copy %s\n", refDirEntry->d_name);
                         returnValue = -1;
