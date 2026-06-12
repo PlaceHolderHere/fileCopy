42241a0471d9dfe18855d22da6f3f8e0ef98268e
PlaceHolderHere <holderhereinplace@gmail.com>
2026-06-12 10:31:42 +0800

    Made a new folder (src) for all .c and .h files, and added a new file (remove.c)

---
+++ src/remove.c
@@ -0,0 +1,17 @@
+#define _FILE_OFFSET_BITS 64
+
+int rmDirWindows(char directoryPath[]);
+int rmDirLinux(char directoryPath[]);
+int rmDirMac(char directoryPath[]);
+
+int rmDirWindows(char directoryPath[]){
+
+}
+
+int rmDirLinux(char directoryPath[]){
+    
+}
+
+int rmDirMac(char directoryPath[]){
+    
+}
